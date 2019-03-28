d5a0803508187669d18ba007dc06bf98b7c53bb8
Alexandr <42433341+Lodegal@users.noreply.github.com>
2019-03-28 16:49:22 +0200

    Updates

---
+++ precache-manifest.4e6cdb20700296d12f36cb738362441a.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = [
+  {
+    "revision": "fcfdeb87f9dcf3138146a189b715528a",
+    "url": "/Lodegal/react-portfolio/static/media/tajam.fcfdeb87.png"
+  },
+  {
+    "revision": "67be987e239dac4b3875",
+    "url": "/Lodegal/react-portfolio/static/css/main.1c872a77.chunk.css"
+  },
+  {
+    "revision": "b1362455b16d0c6ae8e7",
+    "url": "/Lodegal/react-portfolio/static/js/runtime~main.1795e47d.js"
+  },
+  {
+    "revision": "177c6033f18fe401181d3a4bc604166b",
+    "url": "/Lodegal/react-portfolio/static/media/my-photo.177c6033.jpg"
+  },
+  {
+    "revision": "553c062fce2228ba5d34",
+    "url": "/Lodegal/react-portfolio/static/js/2.4ccc0090.chunk.js"
+  },
+  {
+    "revision": "06f25ca4be460f95360e55860ecf7ac2",
+    "url": "/Lodegal/react-portfolio/static/media/Barber.06f25ca4.png"
+  },
+  {
+    "revision": "f112d098f0f3272f7b867ccf1e508684",
+    "url": "/Lodegal/react-portfolio/static/media/Awax.f112d098.png"
+  },
+  {
+    "revision": "67be987e239dac4b3875",
+    "url": "/Lodegal/react-portfolio/static/js/main.7394517d.chunk.js"
+  },
+  {
+    "revision": "1959fb74efb970bf95a6ba61af438a6f",
+    "url": "/Lodegal/react-portfolio/static/media/Idea.1959fb74.png"
+  },
+  {
+    "revision": "2df4c8dbe4eb952a776f840cf9d93a13",
+    "url": "/Lodegal/react-portfolio/static/media/Orbitron-Regular.2df4c8db.woff2"
+  },
+  {
+    "revision": "c801dd36d6167008f7e595029a7d65f3",
+    "url": "/Lodegal/react-portfolio/static/media/Orbitron-Regular.c801dd36.woff"
+  },
+  {
+    "revision": "371fd6554f03806f94d2b81b049894c1",
+    "url": "/Lodegal/react-portfolio/static/media/Orbitron-Regular.371fd655.ttf"
+  },
+  {
+    "revision": "20838451d5de8465cc437511aa7e8cab",
+    "url": "/Lodegal/react-portfolio/static/media/Orbitron-Regular.20838451.eot"
+  },
+  {
+    "revision": "553c062fce2228ba5d34",
+    "url": "/Lodegal/react-portfolio/static/css/2.1b264c32.chunk.css"
+  },
+  {
+    "revision": "d6a91f8ff9c91547e37c593aff29c7ee",
+    "url": "/Lodegal/react-portfolio/index.html"
+  }
+];
